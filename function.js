292914c1382d51146a6bcfaecd2df6e856cbeec2
utshomomen <mohibulmomenutsho@yahoo.com>
2020-01-27 07:03:14 +0300

    function.js

---
+++ function.js
@@ -0,0 +1,14 @@
+function mysexlife (){
+
+
+    for( var yellow=0;yellow<=5;yellow++)
+        {
+            if(yellow=true){
+                console.log("you are an asswhole")
+            }
+        }
+
+
+}
+let total= mysexlife();
+console.log(total);
