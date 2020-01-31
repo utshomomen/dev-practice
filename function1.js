af0a0a6085e9d4c9fb1de4d57ab455fa66b66723
utshomomen <mohibulmomenutsho@yahoo.com>
2020-02-01 01:01:18 +0300

    new function-DEV

---
+++ function1.js
@@ -0,0 +1,14 @@
+
+function calculate_sales(tax,price){
+
+    console.log("tax " + tax );
+    console.log("price " + price);
+    return tax*price;
+    
+    
+    }
+    
+    
+    var newcalculate_sales = calculate_sales(10,50);
+    
+    console.log("sales tax's are $ " + newcalculate_sales);
