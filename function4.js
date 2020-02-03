46d98c9741e99091d47454a5c9c936c5c725a485
utshomomen <mohibulmomenutsho@yahoo.com>
2020-02-03 04:33:39 +0300

    new function with insipired by bucky's c++ tutorial

---
+++ function4.js
@@ -0,0 +1,24 @@
+function buckysstyle(motherfucker, sisterfucker){
+
+let result = motherfucker * sisterfucker;
+
+for(let i =0; i<5;i++){
+
+   
+
+    console.log("Iteration " + i + " uses outerVar " + result + ".");
+    if (i % 5 === 0) {
+      result++;
+    }
+
+   
+}
+
+
+
+
+}
+
+
+console.log(buckysstle(22, 69))
+
